93102d54b45ae610c9e6d14276be2bb7e068ac61
youkidkk <youkidkk80@gmail.com>
2017-10-07 10:10:56 +0900

    コメント追加

--- JMockitSample/src/test/java/jmockit/sample/ConstructorMockSample.java
+++ JMockitSample/src/test/java/jmockit/sample/ConstructorMockSample.java
@@ -1,5 +1,7 @@
 package jmockit.sample;
 
+import static org.junit.Assert.*;
+
 import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Test;
@@ -7,10 +9,11 @@ import org.junit.Test;
 import java.io.UnsupportedEncodingException;
 
 /**
- *
+ * コンストラクタモック化サンプル。
  */
 public class ConstructorMockSample {
 
+    @SuppressWarnings("javadoc")
     public static class Hoge {
 
         private int fuga;
@@ -27,16 +30,21 @@ public class ConstructorMockSample {
         }
     }
 
+    /**
+     * コンストラクタのモック化。
+     */
     @Test(expected = UnsupportedEncodingException.class)
     public void test() {
-        new MockUp<Hoge>() {
+        new MockUp<Hoge>() { // 対象の方を指定する
 
+            // メソッド名を $init にして、引数を対象のコンストラクタと合わせる
             @Mock
             public void $init(int fuga) throws UnsupportedEncodingException {
                 throw new UnsupportedEncodingException();
             }
         };
-        Hoge hoge = new Hoge(123);
+        Hoge hoge = new Hoge(123); // UnsupportedEncodingException発生
+        fail(hoge.toString());
     }
 
 }
