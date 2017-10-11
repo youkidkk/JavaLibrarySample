c346ba425bf3ca156521a947e92104dabb7fe681
youkidkk <youkidkk80@gmail.com>
2017-10-11 18:19:30 +0900

    バージョン1.30でのコンストラクタのモック化のサンプルを追加

--- JMockitSample/src/test/java/jmockit/sample/ConstructorMockSample.java
+++ JMockitSample/src/test/java/jmockit/sample/ConstructorMockSample.java
@@ -1,11 +1,15 @@
 package jmockit.sample;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
+import mockit.Mocked;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
@@ -20,15 +24,19 @@ public class ConstructorMockSample {
     @SuppressWarnings("javadoc")
     public static class Hoge {
 
+        public Hoge() {
+        }
+
         private int fuga;
 
     }
 
     /**
-     * コンストラクタのモック化。
+     * コンストラクタのモック化（バージョン1.28の場合）。
+     * 例外をスロー出来るだけ。
      */
     @Test(expected = UnsupportedEncodingException.class)
-    public void test() {
+    public void test1() {
         new MockUp<Hoge>() { // 対象の方を指定する
 
             // メソッド名を $init にして、引数を対象のコンストラクタと合わせる
@@ -39,6 +47,27 @@ public class ConstructorMockSample {
         };
         Hoge hoge = new Hoge(123); // UnsupportedEncodingException発生
         fail(hoge.toString());
+    }
+
+    /**
+     * コンストラクタのモック化（バージョン1.30の場合）。
+     */
+    @Test
+    public void test2(@Mocked Hoge hogeMock) {
+        new Expectations() {
+
+            {
+                new Hoge(123);
+                this.result = hogeMock;
+                this.times = 1;
+                hogeMock.getFuga();
+                this.result = 321;
+                this.times = 1;
+            }
+        };
+        Hoge hogeInstance = new Hoge(123);
+        assertThat(hogeInstance, sameInstance(hogeInstance));
+        assertThat(hogeInstance.getFuga(), is(321));
     }
 
 }
